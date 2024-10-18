88b27c8265fd457622662b413e78c4c0f45dfcab
Sarah Pickering <sarahpsellsaz@gmail.com>
2024-10-18 14:04:23 -0700

    create Axios Response Error method to clean code

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -13,34 +13,36 @@ export default class App extends React.Component {
     const { value } = evt.target
     this.setState({ ...this.state, todoNameInput: value })
   }
+  resetForm= () => this.setState({ ...this.state, todoNameInput: '' })
+
+  setAxiosResponseError = err => this.setState({...this.state, error: err.response.data.message })
 
   postNewTodo = () => {
     axios.post(URL, { name: this.state.todoNameInput })
     .then(res => {
       this.fetchAllTodos()
+      this.resetForm()
     })
-    .catch(err => {
-      this.setState({ ...this.state, error: err.response.data.message })
-    })
+    .catch(this.setAxiosResponseError)
   }
+
   onTodoFormSubmit = evt => {
     evt.preventDefault()
     this.postNewTodo()
   }
+
   fetchAllTodos = () => {
     axios.get(URL)
       .then(res => {
         this.setState({ ...this.state, todos: res.data.data })
       })
-      .catch(err => {
-        this.setState({ ...this.state, error: err.response.data.message })
-      })
+      .catch(this.setAxiosResponseError)
   }
 
- 
   componentDidMount() {
     this.fetchAllTodos()
   }
+
   render() {
     return (
       <div>
